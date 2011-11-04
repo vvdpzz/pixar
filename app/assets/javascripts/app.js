7be419dc5593cac3c7cd5c80b107cc405ec1e553
Li Yuan <202070317@163.com>
2011-11-04 16:16:48 +0800

    nav

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -119,4 +119,98 @@ ce6.site = {
 		else
 			window.location.href = this.url(method, params);
     }
-}
+};
+
+(function (a) {
+    function b(f) {
+        var g = {},
+            m = /^jQuery\d+$/;
+        a.each(f.attributes, function (p, t) {
+            if (t.specified && !m.test(t.name)) g[t.name] = t.value
+        });
+        return g
+    }
+    function e() {
+        var f = a(this);
+        if (f.val() === f.attr("placeholder") && f.hasClass("placeholder")) f.data("placeholder-password") ? f.hide().next().show().focus() : f.val("").removeClass("placeholder")
+    }
+    function h() {
+        var f, g = a(this),
+            m = g,
+            p = g.data("placeholder-init");
+        if (g.val() === "" || !p && g.val() === g.attr("placeholder")) {
+            if (g.is(":password")) {
+                if (!g.data("placeholder-textinput")) {
+                    try {
+                        f = g.clone().attr({
+                            type: "text"
+                        })
+                    } catch (t) {
+                        f = a("<input>").attr(a.extend(b(g[0]), {
+                            type: "text"
+                        }))
+                    }
+                    f.removeAttr("id").removeAttr("name").data("placeholder-password", true).bind("focus.placeholder", e);
+                    g.data("placeholder-textinput", f).before(f)
+                }
+                g = g.hide().prev().show()
+            }
+            g.addClass("placeholder").val(g.attr("placeholder"))
+        } else g.removeClass("placeholder");
+        p || m.data("placeholder-init", true)
+    }
+    a.fn.placeholder = function () {
+        return this.filter(":input[placeholder]").bind("focus.placeholder", e).bind("blur.placeholder", h).trigger("blur.placeholder").end()
+    };
+    a(function () {
+        a("form").bind("submit.placeholder", function () {
+            var f = a(".placeholder", this).each(e);
+            setTimeout(function () {
+                f.each(h)
+            }, 10)
+        })
+    });
+    a(window).bind("unload.placeholder", function () {
+        a(".placeholder").val("")
+    })
+})(jQuery);
+
+(function (a) {
+    var b = {
+        button: null,
+        menu: null
+    },
+        e = function (f, g, m) {
+            f.click(function (p) {
+                if (b.menu != g) {
+                    h();
+                    f.removeClass("inactive-menu").addClass("active-menu");
+                    g.show();
+                    var t = g.data("onMenuShow");
+                    t && t(g);
+                    b.button = f;
+                    b.menu = g
+                } else h();
+                p.stopPropagation()
+            });
+            m = m || {};
+            m.onMenuShow && g.data("onMenuShow", m.onMenuShow);
+            m.onMenuHide && g.data("onMenuHide", m.onMenuHide)
+        },
+        h = function () {
+            if (b.button) {
+                b.button.removeClass("active-menu").addClass("inactive-menu");
+                b.menu.hide();
+                var f = b.menu.data("onMenuHide");
+                f && f(b.menu)
+            }
+            b.button = b.menu = null
+        };
+    a(document).click(h);
+    a.fn.extend({
+        toggleMenu: function (f, g) {
+            e(a(this), f, g);
+            return this
+        }
+    })
+})(jQuery);
